669c6376ef632c571262b56da3db0ab4780234d5
Loststray <60587418+Loststray@users.noreply.github.com>
2023-03-02 17:26:55 +0800

    Update snake.h

--- Chapter7/snake.h
+++ Chapter7/snake.h
@@ -4,12 +4,13 @@ const int MAP_HEIGHT = 30;
 #define UP 'w'
 #define DOWN 's'
 #define LEFT 'a'
-void Topos(const int16_t ,const int16_t );
+void Topos(const int32_t ,const int32_t );
 void Help();
 void About();
 void InitMAP();
 bool Move_Snake();
 void Speed_Control();
 void Generate_Food();
+void Generate_Special_Food();
 void Hide();
 int16_t Menu();
