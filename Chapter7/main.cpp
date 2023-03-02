d3014b6aaa3a1981136e9a831f8e31304bcf514c
Loststray <60587418+Loststray@users.noreply.github.com>
2023-03-02 17:22:44 +0800

    Update main.cpp

--- Chapter7/main.cpp
+++ Chapter7/main.cpp
@@ -3,26 +3,36 @@
 #include <conio.h>
 #include "snake.h"
 typedef std::pair<int32_t ,int32_t > pos;
-int32_t G[MAP_HEIGHT + 2][MAP_WIDTH + 2];
+int32_t G[MAP_HEIGHT + 2][MAP_WIDTH + 2],step = 0;
+pos Special_Food;
 class SNAKE
 {
 public:
-    int32_t speed,len;
+    int32_t speed,len,cnt;
     char dir = 'd';
     std::deque<pos> q;
-    SNAKE(int s,int l) { speed = s,len = l; }
+    SNAKE(int s,int l) { speed = s,len = l,cnt = 0; }
     void init()
     {
         len = 3;
         while(!q.empty())
             q.pop_front();
+        cnt = 0;
         dir = 'd';
     }
-    bool push_pos(int x,int y)
+    bool push_pos(int32_t &x,int32_t &y)
     {
-        if (G[x][y] == 1 || x <= 0 || x >= MAP_WIDTH || y <= 0 || y >= MAP_HEIGHT)
+        if (G[x][y] == 1)
             return false;
-        if (q.size())
+        if (x == 0)
+            x = MAP_WIDTH - 1;
+        if (x == MAP_WIDTH)
+            x = 1;
+        if (y == 0)
+            y = MAP_HEIGHT - 1;
+        if (y == MAP_HEIGHT)
+            y = 1;
+        if (!q.empty())
         {
             auto pre = q.back();
             Topos(pre.first,pre.second);
@@ -31,11 +41,15 @@ public:
         q.emplace_back(x,y);
         if (G[x][y])
         {
+            len += G[x][y] - 1;
+            cnt++;
+            if (cnt % 2 == 0)
+                Generate_Special_Food();
+            if (G[x][y] <= 2)
+                Generate_Food();
             G[x][y] = 1;
             Topos(x,y);
             std::cout << '@';
-            len++;
-            Generate_Food();
         }
         else
         {
@@ -73,15 +87,17 @@ int main() {
             case 3:
                 About();
                 break;
+            case 4:
+
             default:
                 exit(0);
         }
     }
 }
-void Topos(const int16_t x,const int16_t y)
+void Topos(const int32_t x,const int32_t y)
 {
     HANDLE stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD cursorPosition = {x, y};
+    COORD cursorPosition = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
     SetConsoleCursorPosition(stdHandle, cursorPosition);
 }
 void Hide()
@@ -101,7 +117,7 @@ int16_t Menu()
     Topos(43,18);
     std::cout << "3.关于";
     Topos(43,20);
-    std::cout << "4.";
+    std::cout << "4.退出";
     Hide();
     char ch = _getch();
     system("cls");
@@ -124,6 +140,22 @@ void Help()
     char ch;
     ch = _getch();
     system("cls");
+}
+void Generate_Special_Food()
+{
+    while(true)
+    {
+        int32_t x = rand() % 21 - 10, y = rand() % 21 - 10;
+        x += s.q.back().first, y += s.q.back().second;
+        if (x > 0 && x < MAP_WIDTH && y > 0 && y < MAP_HEIGHT && !G[x][y])
+        {
+            G[x][y] = 6;
+            Special_Food = {x,y};
+            Topos(x,y);
+            std::cout << '*';
+            return ;
+        }
+    }
 }
 void Generate_Food()
 {
@@ -167,7 +199,7 @@ void InitMAP()
 void Speed_Control()
 {
     if (s.len <= 30)
-    s.speed = 240 - (s.len / 3) * 20;
+    s.speed = 260 - (s.len / 3) * 20;
 }
 bool Move_Snake()
 {
@@ -228,10 +260,32 @@ bool Move_Snake()
         std::cout << "ded";
         char tmp = _getch();
         system("cls");
-        return false;
+            return false;
+        }
+        Topos(40,5);
+        std::cout << "SCORE :" << s.len - 3;
+        if (G[Special_Food.first][Special_Food.second] >= 3)
+        {
+            step++;
+            if (step % 5 == 0)
+                G[Special_Food.first][Special_Food.second]--;
+            Topos(40,8);
+            std::cout << "特殊果子还有" << 20 - step << "步就要逃走啦 ";
+            if (step >= 20)
+            {
+                Topos(Special_Food.first,Special_Food.second);
+                std::cout << ' ';
+                Topos(40,8);
+                std::cout << "                                    ";
+                G[Special_Food.first][Special_Food.second] = 0;
+        }
     }
-    Topos(40,5);
-    std::cout << "SCORE :" << s.len - 3;
+    else
+    {
+        Topos(40,8);
+        std::cout << "                                    ";
+        step = 0;
+    }
     Speed_Control();
     Sleep(s.speed);
     return true;
